9157afdd97943d62a272c5061ebdb5650f2d4726
Krinishas7 <krinishashrestha7@gmail.com>
2025-02-20 21:51:08 +0545

    Update delete_course.php

--- pages/delete_course.php
+++ pages/delete_course.php
@@ -16,7 +16,7 @@ if (!isset($_GET['id'])) {
 
 $courseId = $_GET['id'];
 
-// Delete the course from the database
+// following code is database deletion 
 $sql = "DELETE FROM courses WHERE id = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param('i', $courseId);
